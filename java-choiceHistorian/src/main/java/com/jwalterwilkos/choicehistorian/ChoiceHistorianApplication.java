d50bdba95d548b92203b42265a95dca625b6bbdc
John Wilkos <johnwilkosmail@gmail.com>
2021-10-12 16:57:23 +0200

    Initial commit. Empty Java and Angular projects generated.

---
+++ java-choiceHistorian/src/main/java/com/jwalterwilkos/choicehistorian/ChoiceHistorianApplication.java
@@ -0,0 +1,13 @@
+package com.jwalterwilkos.choicehistorian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceHistorianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoiceHistorianApplication.class, args);
+    }
+
+}
---
+++ java-choiceHistorian/src/test/java/com/jwalterwilkos/choicehistorian/ChoiceHistorianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwalterwilkos.choicehistorian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceHistorianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
